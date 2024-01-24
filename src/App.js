a89607ee4b20a087fb51b0c01f7f6dac0a012466
Daniel Táborský <daniel-taborsky77@seznam.cz>
2024-01-24 21:11:34 +0100

    Struktura Navigace připravena

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navigation from "./Components/Navigation";
 
 const App = () =>{
-  
+    
     return (
-      <h1>Test</h1>
+      <div>
+        <Navigation />
+        
+      </div>
+      
 
     )
-  }
-  export default App;
+}
+export default App;
 
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,25 @@
 import "./Navigation.css"
+import Image1 from "../assets/images/Logo.png";
 
 const Navigation = () => {
-    return <h1>Navigace</h1>
+    return (
+        <div className="Nav">
+            
+            <div className="Nav-Left">
+                
+                <a href="" className="Nav_Link"><img src={Image1} alt="" /></a>
+                <a href="" className="Nav_Link"><h1>Carentab</h1></a>            
+                
+            </div>
+            <div className="Nav-Right">
+                <a href="" className="Nav_Link"><h1>Naše vozy</h1></a>
+                <a href="" className="Nav_Link"><h1>Vaše dotazy</h1></a>
+                <a href="" ><button className="Nav_Button">Rezervovat</button></a>
+            </div>
+        </div>
+    )
+        
+    
 } 
 
 export default Navigation
